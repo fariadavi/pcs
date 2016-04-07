e0af1d94dfce219d6abf27ef4bb214134c3edc52
Davi Anjos <d.a.faria@gmail.com>
2016-04-07 15:32:47 -0300

    Ex 10 list 2

---
+++ pcs/src/Lista2/Ex10.java
@@ -0,0 +1,26 @@
+package l2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex10 {
+
+    public static void main(String args[]) {
+        List<List<Integer>> mainList = new ArrayList<List<Integer>>();
+        
+        for(int i = 1; i < 10; i++) {
+            List<Integer> listI = new ArrayList<Integer>();
+            for(int j = i; j <= 100; j+=i)
+                listI.add(j);
+            mainList.add(listI);
+        }
+        
+        int count = 1; 
+        for (List<Integer> list : mainList) {
+            System.out.print(count++ + ": ");
+            for(int i : list)
+                System.out.print(i + " ");
+            System.out.print("\n");
+        }
+    }
+}
